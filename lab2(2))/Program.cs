3a381ed2f89d3eb59b9f950905965d57b9c041e7
chirv <chirv@DESKTOP-VOT2QOU>
2021-10-21 19:49:07 +0200

    Добавьте файлы проекта.

---
+++ lab2(2))/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Lab2_2_
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            double a, b, c;
+            Console.WriteLine("Сторона a = ");
+            a = double.Parse(Console.ReadLine());
+            Console.WriteLine("Сторона b = ");
+            b = double.Parse(Console.ReadLine());
+            Console.WriteLine("Сторона c = ");
+            c = double.Parse(Console.ReadLine());
+            if (a == b) Console.WriteLine("Треугольник является равнобедренным");
+            else if (a == c) Console.WriteLine("Треугольник является равнобедренным");
+            else if (b == c) Console.WriteLine("Треугольник является равнобедренным");
+            else Console.WriteLine("Треугольник не является равнобедренным");
+        }
+    }
+}
